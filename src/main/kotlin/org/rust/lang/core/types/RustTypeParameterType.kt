744f2fbd04943cf381ba592f9a693c7937a4a3a0
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-07-30 13:55:39 +0200

    (MISC): Fixes after rebase

--- src/main/kotlin/org/rust/lang/core/types/RustTypeParameterType.kt
+++ src/main/kotlin/org/rust/lang/core/types/RustTypeParameterType.kt
@@ -5,7 +5,7 @@ import org.rust.lang.core.types.visitors.RustTypeVisitor
 
 class RustTypeParameterType(val parameter: RustTypeParamElement) : RustType {
 
-    override val traits: Sequence<RustTraitItemElement>
+    val traits: Sequence<RustTraitItemElement>
         get() = parameter.bounds.mapNotNull { it.bound.traitRef?.path?.reference?.resolve() as? RustTraitItemElement }
 
     override fun <T> accept(visitor: RustTypeVisitor<T>): T = visitor.visitTypeParameter(this)
